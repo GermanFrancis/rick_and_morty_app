5be8c07bc86e788153baed0a8bf16476835e7251
German <German_francis@hotmail.com>
2024-01-15 17:17:04 -0500

    :lipstick: Add index
    Add rick & morty index

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
+import imageRickAndMorty from "./img/rick-morty.png";
 import './App.css';
 
 function App() {
+
+  const searchCharacter = () => {
+    
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className='title'> Rick & Morty App </h1>
+        <img src={imageRickAndMorty} alt='Rick & Morty' className='img-home'/>
+        <button onClick={searchCharacter} className="btn-search"> Buscar personaje</button>
       </header>
     </div>
   );
