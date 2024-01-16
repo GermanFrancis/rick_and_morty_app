269466d32e02e1cc13e31a146240fd6b53a4fb74
German <German_francis@hotmail.com>
2024-01-15 22:23:31 -0500

    :alien: Api comunication
    Add api comunication

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
 import imageRickAndMorty from "./img/rick-morty.png";
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import { Character } from "./components/Character";
 
 function App() {
+  const [characters, setCharacters] = useState(null);
 
-  const searchCharacter = () => {
-    
-  }
+  const searchCharacter = async () => {
+    const API_URL = await fetch("https://rickandmortyapi.com/api/character");
+    const characterApi = await API_URL.json();
+    setCharacters(characterApi.results);
+  };
 
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className='title'> Rick & Morty App </h1>
-        <img src={imageRickAndMorty} alt='Rick & Morty' className='img-home'/>
-        <button onClick={searchCharacter} className="btn-search"> Buscar personaje</button>
+        <h1 className="title"> Rick & Morty App </h1>
+        {characters ? (
+          <Character characters={characters} setCharacters={setCharacters}/>
+        ) : (
+          <>
+            <img
+              src={imageRickAndMorty}
+              alt="Rick & Morty"
+              className="img-home"
+            />
+            <button onClick={searchCharacter} className="btn-search">
+              Search characters
+            </button>
+          </>
+        )}
       </header>
     </div>
   );
